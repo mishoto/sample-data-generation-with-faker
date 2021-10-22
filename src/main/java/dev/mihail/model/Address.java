33159a149e4d1c70f0c7f35839dec2b96fe76ae8
Mishoto <mihail.dev.io@gmail.com>
2021-10-22 10:35:55 +0300

    create entity model classes

---
+++ src/main/java/dev/mihail/model/Address.java
@@ -0,0 +1,103 @@
+package dev.mihail.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="c_address", schema="data")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = ADDRESS_ID, nullable = false)
+    private Long id;
+    @Column(name = COUNTRY, nullable = false)
+    private String country;
+    @Column(name = CITY, nullable = false)
+    private String city;
+    @Column(name = STREET_NAME, nullable = false)
+    private String streetName;
+    @Column(name = STREET_NUMBER, nullable = false)
+    private String streetNumber;
+    @Column(name = STATE, nullable = false)
+    private String state;
+
+    public static final String ADDRESS_ID = "address_id";
+    public static final String COUNTRY = "country";
+    public static final String CITY = "city";
+    public static final String STREET_NAME = "street_name";
+    public static final String STREET_NUMBER = "street_number";
+    public static final String STATE = "state";
+
+    public Address() {
+    }
+
+    public Address(String country, String city, String streetName, String streetNumber, String state) {
+        this.country = country;
+        this.city = city;
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+        this.state = state;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Address{");
+        sb.append("id=").append(id);
+        sb.append(", country='").append(country).append('\'');
+        sb.append(", city='").append(city).append('\'');
+        sb.append(", streetName='").append(streetName).append('\'');
+        sb.append(", streetNumber='").append(streetNumber).append('\'');
+        sb.append(", state='").append(state).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
+
+
---
+++ src/main/java/dev/mihail/model/Customer.java
@@ -0,0 +1,97 @@
+package dev.mihail.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="customer", schema="data")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = CUSTOMER_ID, nullable = false)
+    private Long id;
+    @Column(name = CUSTOMER_FN, nullable = false)
+    private String firstName;
+    @Column(name = CUSTOMER_LN, nullable = false)
+    private String lastName;
+    @Column(name = CUSTOMER_PHONE, nullable = false)
+    private String phoneNumber;
+    @Column(name = CUSTOMER_EMAIL)
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = C_ADDRESS_ID, referencedColumnName = "address_id")
+    private Address address;
+
+    public static final String CUSTOMER_ID = "customer_id";
+    public static final String CUSTOMER_FN = "customer_fn";
+    public static final String CUSTOMER_LN = "customer_ln";
+    public static final String CUSTOMER_PHONE = "customer_phone";
+    public static final String CUSTOMER_EMAIL = "customer_email";
+    public static final String C_ADDRESS_ID = "c_address_id";
+
+    public Customer(){}
+
+    public Customer(String firstName, String lastName, String phoneNumber, String email, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Customer{");
+        sb.append("id=").append(id);
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", phoneNumber='").append(phoneNumber).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", address=").append(address);
+        sb.append('}');
+        return sb.toString();
+    }
+}
